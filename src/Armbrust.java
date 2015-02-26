f145dfa7fe4cb84bdcfec3fb3345797945c20626
unknown <pbehrendt@ARCC2137.arc.local>
2015-02-26 09:40:33 +0100

    unnötige Klassen löschen

--- src/Armbrust.java
+++
@@ -1,9 +0,0 @@
-import org.newdawn.slick.SlickException;
-
-public class Armbrust{
-
-	public Armbrust(float position_X, float position_Y, float angleToTurn)
-			throws SlickException {
-		// TODO Auto-generated constructor stub
-	}
-}
--- src/Luftschlag.java
+++
@@ -1,9 +0,0 @@
-import org.newdawn.slick.SlickException;
-
-public class Luftschlag {
-
-	public Luftschlag(float position_X, float position_Y, float angleToTurn)
-			throws SlickException {
-		// TODO Auto-generated constructor stub
-	}
-}
